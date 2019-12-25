3aa69e69fcd16fbae879b1958ba4ca491842ce09
Komal Jaiswal <jaiswalkomal18@gmail.com>
2019-12-25 15:44:21 +0530

    python datatype string and number basics

---
+++ venv/Scripts/python-string-number-variable.py
@@ -0,0 +1,24 @@
+#three basic types of data in python
+character_name="forest gump";
+character_age=35.00; #number
+isMale=True; #boolean
+print("hello all I watched the movie actor "+character_name);
+print("Actor age is "+ str(character_age));
+
+#updated the same variable with the same name
+
+character_name="tom cruiz";
+character_age="13";
+print("Mission Impossible actor is "+ character_name);
+
+
+print("subex\nLimited"); #insert new line
+print("new\"york"); #to put quataion mark
+
+#python built in functions
+
+print(character_name.upper());
+print(character_name.isupper());
+#one line
+print(character_name.lower().islower());
+print(len(character_name));
