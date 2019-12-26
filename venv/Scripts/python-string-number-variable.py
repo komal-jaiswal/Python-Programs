b8e7e3508074941e4915bb23809681d2edd00447
Komal Jaiswal <jaiswalkomal18@gmail.com>
2019-12-26 15:11:59 +0530

    some math and string functions

--- venv/Scripts/python-string-number-variable.py
+++ venv/Scripts/python-string-number-variable.py
@@ -1,3 +1,4 @@
+from math import *
 #three basic types of data in python
 character_name="forest gump";
 character_age=35.00; #number
@@ -22,3 +23,21 @@ print(character_name.isupper());
 #one line
 print(character_name.lower().islower());
 print(len(character_name));
+
+#---------------------------------------------more string functions-----------------------------------
+location="Bangalore Karnataka";
+print(location[5]);
+print(location.index("Karn"));
+print(location.replace("Karnataka","India"));
+#--------------------------------------------numbers in python------------------------------------------
+num=-10.78;
+print(num);
+print(num % 3)
+print(abs(num));
+print(pow(3,5));
+print(max(3,5));
+print(round(3.4));
+print(round(4.7));
+print(ceil(3.4))
+print(sqrt(36));
+
