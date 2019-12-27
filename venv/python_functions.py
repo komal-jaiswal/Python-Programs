1ea3d6eb999324e7d88f7925482b7447f2e9ef36
Komal Jaiswal <jaiswalkomal18@gmail.com>
2019-12-27 14:59:07 +0530

    python function syntax

---
+++ venv/python_functions.py
@@ -0,0 +1,14 @@
+def hello():
+    print("Hello World !")
+
+def hello_person(name,age):
+    print("Hello "+name +"your age is  "+ str(age))
+
+hello()
+hello_person("Ruby",10)
+
+def cube(num):
+    return num * num * num
+
+result=cube(2);
+print(result)
